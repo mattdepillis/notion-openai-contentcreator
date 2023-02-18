0e521be557f6de1f307ca3730c093365ed2b368b
mattdepillis <mattdepillis@gmail.com>
2023-02-18 14:09:42 -0500

    function to fetch notion data from the api

--- ui/src/api/notion/notion.js
+++ ui/src/api/notion/notion.js
@@ -1,6 +1,7 @@
 export const fetchNotionData = async () => {
   console.log(process.env.REACT_APP_API_URL)
   const data = await fetch(process.env.REACT_APP_API_URL, { method: 'GET' })
-  console.log('d', data)
-  return data.json()
+    .then(d => d.json())
+    .then(d => d)
+  return data
 }
