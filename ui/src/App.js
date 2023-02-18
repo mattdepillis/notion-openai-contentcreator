9c82a210bce6eccb59988a0c0fb64fcd5b1a1c1d
mattdepillis <mattdepillis@gmail.com>
2023-02-18 01:29:15 -0500

    testing with notion data fetching in App.js before building out UI more completely

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,9 +1,18 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
+
+import { fetchNotionData } from './api/notion/notion'
 
 const App = () => {
+  const [notionData, setNotionData] = useState(null)
+
+  useEffect(() => {
+    const setData = async () => setNotionData(await fetchNotionData())
+    setData()
+  }, [])
+  useEffect(() => console.log('notionData: ', notionData), [notionData])
   return (
     <Fragment>
-      <p color="#000000">Hello from App.js!</p>
+      {notionData ? <p>{notionData}</p> : <p>Nothing to see here...</p>}
     </Fragment>
   )
 }
