3ef7b592e37ffbb547cb24ef38c33846b62db691
mattdepillis <mattdepillis@gmail.com>
2023-02-20 19:15:09 -0500

    restructured app.ts to delegate routes and logic to other dirs + files

--- api/app.ts
+++ api/app.ts
@@ -1,19 +1,14 @@
-import express, { Request, Response } from 'express'
+import express from 'express'
 
 const cors = require('cors')
 require('dotenv').config()
 
-import { findAllRootPages } from './routes/notion/notionGet'
+import NotionRouter from './routes/notion/notionRoutes'
 
 const app = express()
 app.use(cors())
 
-app.get('/', async (req: Request, res: Response) => {
-  const allPages = await findAllRootPages()
-  const rootPages = allPages.filter(page => page.parent.type === 'workspace')
-  console.log(allPages)
-  res.json(rootPages)
-});
+app.use('/notion', NotionRouter)
 
 app.listen(3001, () => {
   console.log('Server running on port 3001')
