ffeb82c837c745df04b194bbb8be8f2a3b0ba3da
mattdepillis <mattdepillis@gmail.com>
2023-02-22 18:35:05 -0500

    new file with interfaces to simplify/modify notion response types

---
+++ api/types/notion/responseTypes.ts
@@ -0,0 +1,18 @@
+import { PartialPageObjectResponse } from '@notionhq/client/build/src/api-endpoints'
+
+export interface PageObjectWithParent extends PartialPageObjectResponse {
+  parent: {
+    type: 'database_id' | 'page_id' | 'workspace'
+    database_id?: 'string'
+    page_id?: 'string'
+    workspace?: 'boolean'
+  }
+}
+
+export interface PageObjectWithProperties extends PageObjectWithParent {
+  properties: {
+    title: {
+      title: string
+    }
+  }
+}
