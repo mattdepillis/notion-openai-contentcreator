0f89e4c4e92a2957f0f7349789e28e36686a4196
mattdepillis <mattdepillis@gmail.com>
2023-02-22 18:28:45 -0500

    added treeController to manage notion workspace tree controller functions

---
+++ api/controllers/notion/treeController.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from 'express'
+
+import * as notionTree from "../../handlers/notion/notionTree"
+
+export const getTree = async (req: Request, res: Response) => {
+  const tree = await notionTree.buildWorkspaceTree()
+  return tree
+}
