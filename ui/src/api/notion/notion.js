5384895cf34863ae28e15621945eba08882836ef
mattdepillis <mattdepillis@gmail.com>
2023-02-22 18:24:29 -0500

    added fetchChildren() and fetchTree() notion api routes

--- ui/src/api/notion/notion.js
+++ ui/src/api/notion/notion.js
@@ -1,3 +1,13 @@
 export const fetchNotionData = async () =>
   await fetch(process.env.REACT_APP_API_URL + "/notion/roots", { method: 'GET' })
     .then(d => d.json())
+
+export const fetchChildren = async (id) =>
+  await fetch(process.env.REACT_APP_API_URL + `/notion/${id}/children`, { method: 'GET' })
+    .then(d => d.json())
+    .then(d => { console.log("children", d); return d; })
+
+export const fetchTree = async () =>
+  await fetch(process.env.REACT_APP_API_URL + "/notion/tree", { method: 'GET' })
+    .then(d => d.json())
+    .then(d => { console.log("tree", d); return d; })
