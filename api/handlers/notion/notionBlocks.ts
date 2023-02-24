ef9979430f33458752c2a101e50b38e949a2e681
mattdepillis <mattdepillis@gmail.com>
2023-02-24 11:59:52 -0500

    refactored getTreeNodeChildBlocks to return correct blocks from filter + new method getChildBlock

--- api/handlers/notion/notionBlocks.ts
+++ api/handlers/notion/notionBlocks.ts
@@ -1,5 +1,8 @@
 import { BlockObjectResponse } from "@notionhq/client/build/src/api-endpoints"
+
 import { notion } from "../../utils/notionClient"
+import { retrievePage } from "../../handlers/notion/notionPages"
+import { retrieveDatabase } from "./notionDatabases"
 
 // retrieves all child blocks for a given parent block with the notion api
 export const getChildBlocks = async(blockId: string, pageSize?: number) =>
@@ -12,7 +15,13 @@ export const getChildBlocks = async(blockId: string, pageSize?: number) =>
 export const getTreeNodeChildBlocks = async (blockId: string) => {
   const childBlocks = await getChildBlocks(blockId)
     .then(allBlocks => allBlocks.results as BlockObjectResponse[])
-  return childBlocks.filter(
-    block => block.type === "child_page" || "child_database"
-  )
+    .then(blockObjs => blockObjs.filter(
+      block => block.type === "child_page" || block.type === "child_database"
+    ))
+  return childBlocks
 }
+
+export const getChildBlock = async (blockType: string, blockId: string) =>
+  blockType === "child_page"
+    ? await retrievePage(blockId)
+    : await retrieveDatabase(blockId)
