e125f06242afb0ab53fbb8b7f98965aa53307ecb
mattdepillis <mattdepillis@gmail.com>
2023-02-24 12:01:54 -0500

    new handler file for database api operations; new method to fetch database by id and return full db resp

---
+++ api/handlers/notion/notionDatabases.ts
@@ -0,0 +1,7 @@
+import { DatabaseObjectResponse } from "@notionhq/client/build/src/api-endpoints"
+import { notion } from "../../utils/notionClient"
+
+export const retrieveDatabase = async (databaseId: string) => {
+  const db = await notion.databases.retrieve({ database_id: databaseId })
+  return db as DatabaseObjectResponse
+}
