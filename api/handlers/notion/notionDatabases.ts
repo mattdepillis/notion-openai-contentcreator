461ffe46165826d1fb3d9ae89d9e766a081d2d73
mattdepillis <mattdepillis@gmail.com>
2023-02-26 21:29:14 -0500

    new function to query notion database content

--- api/handlers/notion/notionDatabases.ts
+++ api/handlers/notion/notionDatabases.ts
@@ -5,3 +5,8 @@ export const retrieveDatabase = async (databaseId: string) => {
   const db = await notion.databases.retrieve({ database_id: databaseId })
   return db as DatabaseObjectResponse
 }
+
+export const queryDatabase = async (databaseId: string) => {
+  const contents = await notion.databases.query({ database_id: databaseId })
+  return contents
+}
