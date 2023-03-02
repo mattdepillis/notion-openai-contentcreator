60f02979458526b437a314e20c5ae3b993242d72
mattdepillis <mattdepillis@gmail.com>
2023-03-02 00:27:38 -0500

    new controller method for generating a singular tree node

--- api/controllers/notion/treeController.ts
+++ api/controllers/notion/treeController.ts
@@ -6,3 +6,10 @@ export const getTree = async (req: Request, res: Response) => {
   const tree = await notionTree.buildWorkspaceTree()
   res.json(tree)
 }
+
+// new method for generating a tree node
+export const getTreeNode = async(req: Request, res: Response) => {
+  const { id, type } = req.params
+  const treeNode = await notionTree.buildTreeNode(id, type)
+  res.json(treeNode)
+}
