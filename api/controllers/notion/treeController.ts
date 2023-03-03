02c9c2edf41dc8760351b27b3e66152222c54246
mattdepillis <mattdepillis@gmail.com>
2023-03-02 19:25:15 -0500

    removed old tree handler function

--- api/controllers/notion/treeController.ts
+++ api/controllers/notion/treeController.ts
@@ -2,11 +2,6 @@ import { Request, Response } from 'express'
 
 import * as notionTree from "../../handlers/notion/notionTree"
 
-export const getTree = async (req: Request, res: Response) => {
-  const tree = await notionTree.buildWorkspaceTree()
-  res.json(tree)
-}
-
 // new method for generating a tree node
 export const getTreeNode = async(req: Request, res: Response) => {
   const { id, type } = req.params
