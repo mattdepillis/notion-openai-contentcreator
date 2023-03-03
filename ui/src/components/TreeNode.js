b9390f0486bd91e33f3c067d21d0aa28ed0f49b8
mattdepillis <mattdepillis@gmail.com>
2023-03-02 19:27:55 -0500

    additional console.logs, comments, state rendering adjustments for tree

--- ui/src/components/TreeNode.js
+++ ui/src/components/TreeNode.js
@@ -1,24 +1,27 @@
-import React, { Fragment, useState } from "react"
+import React, { Fragment, useEffect, useState } from "react"
 
 const TreeNode = ({ tree, node }) => {
-  const [isExpanded, setIsExpanded] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false)
 
   const handleExpansion = (isExpanded) => {
-    setIsExpanded(!isExpanded);
+    setIsExpanded(!isExpanded)
   }
 
+  useEffect(() => console.log("here", node, tree), [node, tree])
+
+  // TODO: figure out a better way to render the tree for my inspection atm
   return (
     <Fragment>
       <div onClick={() => handleExpansion(isExpanded)}>
-        {node.title}
+        {node.title && (<p>{node.title}</p>)}
         {node.children && (
           <button>{isExpanded ? '-' : '+'}</button>
         )}
       </div>
       {isExpanded &&
-        node.children &&
+        node.children.length > 0 &&
         node.children.map(childId => (
-          <TreeNode key={childId} node={tree[childId]} />
+          <TreeNode key={childId} tree={tree} node={tree[childId]} />
         ))}
     </Fragment>
   )
