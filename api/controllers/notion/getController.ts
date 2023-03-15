b832f0d848b8b0bb0808d2d5ce5fbc1570a93bfc
mattdepillis <mattdepillis@gmail.com>
2023-03-15 19:49:59 -0400

    getBlock() function to test grabbing block data for UI route

--- api/controllers/notion/getController.ts
+++ api/controllers/notion/getController.ts
@@ -1,6 +1,10 @@
+import { BlockObjectResponse } from '@notionhq/client/build/src/api-endpoints'
 import { Request, Response } from 'express'
 
+import * as notionBlocks from '../../handlers/notion/notionBlocks'
 import * as notionSearch from "../../handlers/notion/notionSearch"
+
+import { buildTreeNode } from '../../handlers/notion/notionTree'
 
 export const getRoots = async (req: Request, res: Response) => {
   const rootPages = await notionSearch.findAllRootPages()
@@ -10,4 +14,12 @@ export const getRoots = async (req: Request, res: Response) => {
 export const getPageChildren = async (req: Request, res: Response) => {
   const pageChildren = await notionSearch.findPageChildren(req.params.id)
   res.json(pageChildren)
+}
+
+export const getBlock = async (req: Request, res: Response) => {
+  const block = await notionBlocks.getBlock(req.params.id) as BlockObjectResponse
+  // console.log('b', block)
+  const c = await buildTreeNode(block.id, block.type)
+  // console.log('c', c)
+  res.json(block)
 }
