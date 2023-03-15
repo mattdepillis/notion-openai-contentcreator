5c3a4d4d90829ce289effb702f82849723e4cd68
mattdepillis <mattdepillis@gmail.com>
2023-03-15 19:56:20 -0400

    testing of block fetching from TreeContainer

--- ui/src/containers/TreeContainer.js
+++ ui/src/containers/TreeContainer.js
@@ -1,27 +1,28 @@
-import React from 'react'
-
-// import { fetchTree } from '../api/notion/notion'
+import React, { useEffect, useState } from 'react'
 
 import NotionTree from '../components/Tree'
 
+import { fetchBlock } from '../api/notion/notion'
+
 const TreeContainer = () => {
+  const [block, setBlock] = useState({})
 
   const root = { id: "workspace", type: "database" }
-  // const [rootPages, setRootPages] = useState({})
-  
-  // useEffect(() => {
-  //   const setData = async () => {
-  //     const data = await fetchTree()
-  //     setRootPages(data)
-  //   }
-  //   setData()
-  // }, [])
-  // useEffect(() => {
-  //   console.log('notion Tree: ', rootPages)
-  // }, [rootPages])
+
+  useEffect(() => {
+    const getBlock = async () => {
+      // const b = await fetchBlock("5aa17d15-364b-411e-af37-068e284f91b8")
+      // const b = await fetchBlock("d9e7a543-a995-42e5-879a-0e98f25a473f")
+      const b = await fetchBlock("0c22fdd9-4a6d-453b-ac86-dded128acc90")
+      setBlock(b)
+    }
+    getBlock()
+  }, [])
+  useEffect(() => console.log("block", block), [block])
+
   return (
     <div>
-      <p>Tree</p>
+      <h1>Tree</h1>
       <NotionTree root={root} />
     </div>
   )
