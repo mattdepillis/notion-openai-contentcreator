64ea3badc9ff9f12cf8c95c76643cdcadbf4985d
mattdepillis <mattdepillis@gmail.com>
2023-03-21 19:30:35 -0400

    new route for users in api

--- api/routes/notion/notionRoutes.ts
+++ api/routes/notion/notionRoutes.ts
@@ -2,6 +2,7 @@ const express = require('express')
 
 import * as getController from "../../controllers/notion/getController"
 import * as treeController from "../../controllers/notion/treeController"
+import * as usersController from "../../controllers/notion/usersController"
 
 const NotionRouter = express.Router()
 
@@ -10,5 +11,6 @@ NotionRouter.get('/:id/children', getController.getPageChildren)
 NotionRouter.get('/tree/:type/:id', treeController.getTreeNode)
 
 NotionRouter.get('/block/:id', getController.getBlock)
+NotionRouter.get('/users', usersController.getUser)
 
 export default NotionRouter
