e65d794d087d62d97464408e9be0c2e564c01955
mattdepillis <mattdepillis@gmail.com>
2023-03-22 23:03:24 -0400

    removed TreeContainer from root app

--- ui/src/App.js
+++ ui/src/App.js
@@ -2,14 +2,13 @@ import React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 
 import AppContainer from './containers/AppContainer'
-import TreeContainer from './containers/TreeContainer'
+
 import { extendedGlobalTheme } from './styles/chakraGlobal'
 
 const App = () => {
   return (
     <ChakraProvider theme={extendedGlobalTheme}>
       <AppContainer />
-      <TreeContainer />
     </ChakraProvider>
   )
 }
