c2e308f9d3492a4d90a13d3083b3e4603c1d5e3a
mattdepillis <mattdepillis@gmail.com>
2023-03-27 18:03:02 -0400

    refactored search bar for simplicity

--- ui/src/components/SearchBar.js
+++ ui/src/components/SearchBar.js
@@ -1,15 +1,17 @@
+// import module dependencies
+import { useEffect, useState } from 'react'
+import { SearchIcon } from '@chakra-ui/icons'
 import { Input, InputGroup, InputRightElement } from '@chakra-ui/react'
-import { AddIcon } from '@chakra-ui/icons'
 
-const SearchBar = () => {
-  // future state for filtering search results here
-
-  return (
-    <InputGroup>
-      <Input placeholder='Search for pages and databases...' />
-      <InputRightElement children={<AddIcon />} />
-    </InputGroup>
-  )
-}
+/**
+ * 
+ * @param {*} param0
+ * @returns 
+ */
+const SearchBar = ({ setSearchTerm }) =>
+  <InputGroup>
+    <Input placeholder='Search for pages, databases, and actions...' onChange={e => setSearchTerm(e.target.value)}/>
+    <InputRightElement children={<SearchIcon />} onClick/>
+  </InputGroup>
 
 export default SearchBar
