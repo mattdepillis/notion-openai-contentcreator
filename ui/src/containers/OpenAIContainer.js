4b4acb9ec8bf103f35b98bda5ba62df3569c2b05
mattdepillis <mattdepillis@gmail.com>
2023-03-31 18:46:51 -0400

    new test react container for openai response data

---
+++ ui/src/containers/OpenAIContainer.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react'
+
+import { promptDaVinci } from '../api/openai/openai'
+
+const OpenAIContainer = ({ setElementMap }) => {
+  const [result, setResult] = useState(null)
+
+  useEffect(() => {
+    const getResult = async () => {
+      const r = await promptDaVinci("Write a Hello-World function in C++ and return it formatted in a code block")
+      setResult(r)
+    }
+    getResult()
+  }, [])
+  useEffect(() => console.log("result", result), [result])
+
+  return (
+    <div>
+      <h1>OpenAI API Prompt Result:</h1>
+      {result && <p>{result}</p>}
+    </div>
+  )
+}
+
+export default OpenAIContainer
