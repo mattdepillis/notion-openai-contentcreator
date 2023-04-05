cad3e092b6139de17d7bbc9f86f2836054e93b69
mattdepillis <mattdepillis@gmail.com>
2023-04-05 19:32:33 -0400

    commented promptDaVinci api call to save costs for now

--- ui/src/containers/OpenAIContainer.js
+++ ui/src/containers/OpenAIContainer.js
@@ -2,19 +2,18 @@ import { useEffect, useState } from 'react'
 
 import { promptDaVinci } from '../api/openai/openai'
 
-import { formatOpenAiApiRequest } from '../utils/apiFormatters'
-
-const OpenAIContainer = ({ setElementMap }) => {
+const OpenAIContainer = () => {
   const [result, setResult] = useState(null)
 
   useEffect(() => {
     const getResult = async () => {
-      const r = await promptDaVinci("Write a Hello-World function in C++ and return it formatted in a code block")
+      // const r = await promptDaVinci("Write a Hello-World function in C++ and return it formatted in a code block")
+      const r = "result"
       setResult(r)
     }
     getResult()
   }, [])
-  useEffect(() => console.log("result", result, process.env.REACT_APP_DAVINCI_PATH, formatOpenAiApiRequest('POST', 'hello openai!')), [result])
+  useEffect(() => console.log("result", result), [result])
 
   return (
     <div>
