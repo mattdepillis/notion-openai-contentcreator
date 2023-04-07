5fef51ad11af0ea363f106acaa81fb2f9d024d4e
mattdepillis <mattdepillis@gmail.com>
2023-04-07 15:20:17 -0400

    refactored search result rendering on ui in action grid

--- ui/src/components/ActionsGrid.js
+++ ui/src/components/ActionsGrid.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Card, CardHeader, SimpleGrid } from '@chakra-ui/react'
+import { WarningIcon } from '@chakra-ui/icons'
 
 import { filterKeysByLevenshteinDistance } from '../utils/search'
 
@@ -25,10 +26,15 @@ const ActionsGrid = ({ elementMap, searchTerm }) => {
   }, [elementMap])
 
   useEffect(() => {
+    /*
+      - filter keys by LD from the search term and take the 5 or fewer closest
+      - map elements to an array of [name, blockId]
+    */
     if (searchTerm.length > 0) {
       const st = searchTerm.toLowerCase()
-      const partialMatches = filterKeysByLevenshteinDistance(elementMap, st, 2)
-      setOptions(partialMatches)
+      const partialMatches = filterKeysByLevenshteinDistance(elementMap, st, 2).slice(0, 5)
+      const elements = partialMatches.map(match => [match[0], elementMap[match]])
+      setOptions(elements)
     } else setOptions(Object.entries(elementMap).slice(0, 5))
   }, [elementMap, searchTerm])
 
@@ -41,7 +47,11 @@ const ActionsGrid = ({ elementMap, searchTerm }) => {
           </Card>
         )
       }
-      
+      {(options.length === 0 && searchTerm.length > 0) &&
+        <Card key='no-results-found'>
+          <CardHeader>{<WarningIcon />}  {"Your search term doesn't match any results"}</CardHeader>
+        </Card>
+      }
     </SimpleGrid>
   )
 }
