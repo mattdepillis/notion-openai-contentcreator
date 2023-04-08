e1e171035ce28d92ac43b03bb46b59c35b49810b
mattdepillis <mattdepillis@gmail.com>
2023-04-08 17:14:16 -0400

    refactored into 2 files

--- ui/src/components/actions/ActionGrid.js
+++ ui/src/components/actions/ActionGrid.js
@@ -1,6 +1,8 @@
-import { Button, Card, CardBody, CardHeader, CardFooter, Heading, Text, SimpleGrid } from '@chakra-ui/react'
+import { SimpleGrid } from '@chakra-ui/react'
 
-import { gridActions } from '../../utils/gridActions'
+import ActionCard from './ActionCard'
+
+import { gridActions } from './gridActions'
 
 /**
  * 
@@ -8,21 +10,10 @@ import { gridActions } from '../../utils/gridActions'
  * @returns 
  */
 const ActionGrid = () => {
-
   return (
-    <SimpleGrid columns={4} spacing={10}>
+    <SimpleGrid columns={4} spacing={5}>
       {gridActions.map(action => (
-        <Card id={action.id}>
-          <CardHeader style={{ border: '1px solid red' }}>
-            <Heading size='md'>{action.name}</Heading>
-          </CardHeader>
-          <CardBody>
-            <Text>{action.description}</Text>
-          </CardBody>
-          <CardFooter style={{ border: '1px solid blue' }}>
-            <Button>Go!</Button>
-          </CardFooter>
-        </Card>
+        <ActionCard action={action} />
       ))}
     </SimpleGrid>
   )
