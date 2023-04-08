1ea8c6ee6fc6f4d0097413dad2ab0abf4a605fe9
mattdepillis <mattdepillis@gmail.com>
2023-04-08 16:36:29 -0400

    updated import

--- ui/src/containers/TreeContainer.js
+++ ui/src/containers/TreeContainer.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 
-import NotionTree from '../components/NotionTree'
+import NotionTree from '../components/notionTree/NotionTree'
 
 import { fetchBlock } from '../api/notion/notion'
 
