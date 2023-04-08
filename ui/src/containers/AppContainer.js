e2585a818ed594f8be6a6488ac935ac55415cf12
mattdepillis <mattdepillis@gmail.com>
2023-04-08 16:36:03 -0400

    added new action grid; planning for action state var

--- ui/src/containers/AppContainer.js
+++ ui/src/containers/AppContainer.js
@@ -5,12 +5,16 @@ import { fetchUsers } from '../api/notion/notion'
 
 import { Name, WelcomeContainer } from '../styles/AppContainerStyles'
 
-import SearchBar from '../components/SearchBar'
-
-import ActionsGrid from '../components/ActionsGrid'
-
+// containers
+// TODO: delete OpenAI container; setup client in ~/api
 import OpenAIContainer from './OpenAIContainer'
 import TreeContainer from './TreeContainer'
+
+import ActionGrid from '../components/actions/ActionGrid'
+
+// search components
+import SearchBar from '../components/search/SearchBar'
+import SearchResults from '../components/search/SearchResults'
 
 const AppContainer = () => {
   // state mapping for application user
@@ -19,6 +23,8 @@ const AppContainer = () => {
   const [elementMap, setElementMap] = useState({})
   // state mapping for search term values
   const [searchTerm, setSearchTerm] = useState("")
+  // TODO state mapping for action
+  // const [action, setAction] = useState("")
   
 
   useEffect(() => {
@@ -40,11 +46,15 @@ const AppContainer = () => {
           <Avatar src={user.avatar_url} size='xl' border='2px solid #38A169'>
             <AvatarBadge boxSize='1em' bg='green.500' borderColor='green.100' />
           </Avatar>
-          <Name>Welcome, <strong>{user.name}</strong></Name>
+          <Name>Hello, <strong>{user.name}</strong></Name>
         </WelcomeContainer>
       }
       <SearchBar setSearchTerm={setSearchTerm} />
-      <ActionsGrid elementMap={elementMap} searchTerm={searchTerm} />
+
+      {/* ----- ACTION STATE SHOULD TOGGLE DISPLAY ----- */}
+      <ActionGrid />
+      <br />
+      <SearchResults elementMap={elementMap} searchTerm={searchTerm} />
       <TreeContainer setElementMap={setElementMap} />
       <OpenAIContainer />
     </Container>
