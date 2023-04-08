19a700540e91533352b3fd593031e5de8e777055
mattdepillis <mattdepillis@gmail.com>
2023-04-08 16:37:30 -0400

    new styles for chakra components; custom accordion styles

--- ui/src/styles/chakraComponentStyles.js
+++ ui/src/styles/chakraComponentStyles.js
@@ -3,25 +3,27 @@ import styled from "styled-components"
 // styling for chakra accordion components
 import { Accordion, AccordionButton, AccordionItem } from "@chakra-ui/react"
 
-export const AccordionContainer = styled.div`
-  border-weight: 2px;
-  border-radius: 10px;
-  border-color: red;
-  background-color: dodgerblue;
-`
 export const StyledAccordion = styled(Accordion)`
   border-radius: 10px;
-`
-
-export const StyledAccordionItem = styled(AccordionItem)`
-  border-radius: 10px;
+  margin: 5px;
 `
 
 export const StyledAccordionButton = styled(AccordionButton)`
   opacity: 1;
   border-radius: 10px;
   &[disabled] {
+    background-color: #FFFFFF;
+    cursor: pointer;
+  }
+  :hover:not([disabled]) {
     background-color: #F9F9F1;
   }
 `
 
+export const StyledAccordionItem = styled(AccordionItem)`
+  border-radius: 10px;
+  ::before,
+  ::after {
+    border: 4px solid --chakra-colors-chakra-border-color;
+  }
+`
