39f78e0927672adb2387316045e197480616a849
mattdepillis <mattdepillis@gmail.com>
2023-04-08 17:13:54 -0400

    new action card component to separate jsx from grid

---
+++ ui/src/components/actions/ActionCard.js
@@ -0,0 +1,21 @@
+import { Button, Card, CardBody, CardHeader, CardFooter, Heading, Text } from '@chakra-ui/react'
+
+/**
+ * 
+ * @param {} param0
+ * @returns 
+ */
+const ActionCard = ({ action }) =>
+  <Card id={action.id}>
+    <CardHeader style={{ border: '1px solid red' }}>
+      <Heading size='md'>{action.name}</Heading>
+    </CardHeader>
+    <CardBody>
+      <Text>{action.description}</Text>
+    </CardBody>
+    <CardFooter style={{ border: '1px solid blue' }}>
+      <Button colorScheme='facebook' style={{ width: 'fit-content', margin: 'auto' }}>{action.buttonText}</Button>
+    </CardFooter>
+  </Card>
+
+export default ActionCard
