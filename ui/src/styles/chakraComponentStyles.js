3c3e3df6f949abb060460a8c6d4baae060b4e5ea
mattdepillis <mattdepillis@gmail.com>
2023-04-09 14:33:40 -0400

    styling changes

--- ui/src/styles/chakraComponentStyles.js
+++ ui/src/styles/chakraComponentStyles.js
@@ -1,13 +1,18 @@
 import styled from "styled-components"
 
-// styling for chakra accordion components
-import { Accordion, AccordionButton, AccordionItem } from "@chakra-ui/react"
+import {
+  Accordion, AccordionButton, AccordionItem, // accordion components
+  Heading
+} from "@chakra-ui/react"
 
+
+/* 
+  * Chakra Accordion styles
+*/
 export const StyledAccordion = styled(Accordion)`
   border-radius: 10px;
   margin: 5px;
 `
-
 export const StyledAccordionButton = styled(AccordionButton)`
   opacity: 1;
   border-radius: 10px;
@@ -19,11 +24,17 @@ export const StyledAccordionButton = styled(AccordionButton)`
     background-color: #F9F9F1;
   }
 `
-
 export const StyledAccordionItem = styled(AccordionItem)`
   border-radius: 10px;
   ::before,
   ::after {
     border: 4px solid --chakra-colors-chakra-border-color;
   }
+`
+
+/* 
+  * Chakra Heading styles
+*/
+export const StyledHeading = styled(Heading)`
+  font-size: 1.6em;
 `
