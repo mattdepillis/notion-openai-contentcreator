744a4704483e0d9b45bdf30f0798d5cdb710d801
mattdepillis <mattdepillis@gmail.com>
2023-04-11 18:40:43 -0400

    styles for grid container -- responsive

--- ui/src/components/actions/styles.js
+++ ui/src/components/actions/styles.js
@@ -1,2 +1,26 @@
 import styled from "styled-components"
 
+// grid container
+export const GridContainer = styled.div`
+  .grid {
+    display: grid;
+  }
+  .grid-1 {
+    grid-template-columns: repeat(1, 1fr);
+  }
+  .grid-2 {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  .grid-3 {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  .grid-4 {
+    grid-template-columns: repeat(4, 1fr);
+  }
+`
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 2%;
+`
